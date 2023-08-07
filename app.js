5c21de7b6be3c9cc109652d9608ced16db7f6523
SanathAbdulManaf <sanaabdulmanaf88@gmail.com>
2023-08-07 12:40:05 +0400

    folders are structured in a way that routers are in one folder schema or models are another folder like that

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app=express();
 const bodyParser=require('body-parser');
 const morgan=require('morgan');
 const mongoose=require('mongoose');
+//const Product=require('./models/product');
+const productsRouter=require('./routers/products')
 
 const PORT=process.env.PORT || 3000;
 
@@ -15,44 +17,9 @@ const api=process.env.API_URL
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-           },
-    countInStock:{
-        type:Number,
-        required:true
-    }
-})
+app.use(`${api}/products`,productsRouter);
 
-const Product=mongoose.model('Product',productSchema);
-app.get(`${api}/products`,async (req,res)=>{
-   const productList=await Product.find()
-   if(!productList){
-    res.status(550).json({success:false})
-   }
-    res.send(productList);
-})
-app.post(`${api}/products`,(req,res)=>{
-   const product=new Product({
-    name:req.body.name,
-    image:req.body.image,
-    countInStock:req.body.countInStock
-   })
-   product.save().then((createdProduct=>{
-    res.status(201).json(createdProduct);
-   }))
-   .catch((err)=>{
-    res.status(500).json({
-    error:err,
-    success:false
-    })
-   })
-})
+
 mongoose.connect('mongodb://127.0.0.1:27017/eshopdb-Practice')
 .then(()=>{
     console.log("Database connection is ready");
